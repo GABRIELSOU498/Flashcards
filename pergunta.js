eb24517c26ec11b22b4cbf0dba351f74cb2f595c
GABRIELSOU498 <00001121025390sp@al.educacao.sp.gov.br>
2024-11-20 21:26:12 -0300

    arquivos dos cartões

---
+++ pergunta.js
@@ -0,0 +1,29 @@
+criaCartao (
+    'Programação',
+    'O que é JavaScript?',
+    'É uma linguagem de programação'
+)
+
+criaCartao (
+    'Matemática',
+    'O que é média ponderada?',
+    'É um calcúlo que consiste na soma dos elementos vezes o peso dividindo tudo pela soma dos pesos'
+)
+
+criaCartao (
+    'Geografia',
+    'O que é lixiviação?',
+    'É um processo de retirada de nutrientes do solo por diversos agentes naturais, especialmente a água'
+)
+
+criaCartao (
+    'Programação',
+    'Qual o comando que muda a cor do botão quando passo o mouse?',
+    'Hover'
+)
+
+criaCartao (
+    'Programação',
+    'Como se defini cor de fundo?',
+    'Background-color'
+)
